b539c51d8e2631e07c61bad9f893fb93a3881262
Ana Silva <anajnsilva@gmail.com>
2025-03-12 18:15:01 -0700

    Made favourite page populate fridge cards based on user favourites values from firestore data

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,42 +1,45 @@
-var currentUser;               //points to the document of the user who is logged in
+var currentUser;               
 function populateUserInfo() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if user is signed in:
         if (user) {
 
-            //go to the correct user document by referencing to the user uid
+            
             currentUser = db.collection("users").doc(user.uid)
-            //get the document for current user.
+            
             currentUser.get()
                 .then(userDoc => {
-                    //get the data fields of the user
+                    
                     let userName = userDoc.data().name;
+                    userDoc.data().notify-on-donate ; true;
+                    userDoc.data().notify-on-take ; false;
                 
                     if (userName != null) {
                         document.getElementById("nameInput").value = userName;
                     }
                 })
         } else {
-            // No user is signed in.
+            
             console.log("No user is signed in");
         }
     });
 }
 
-//call the function to run it 
+
 populateUserInfo();
 
 function editUserInfo() {
-    //Enable the form fields
+    
     document.getElementById('personalInfoFields').disabled = false;
 }
 
 function saveUserInfo() {
 
-    userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
+    userName = document.getElementById('nameInput').value;      
 
     currentUser.update({
         name: userName,
+        
     })
     .then(() => {
         console.log("Document successfully updated!");
