42c6d8d096fbc54a978675cd7fb7042a73711591
Ana Silva <anajnsilva@gmail.com>
2025-03-16 18:10:29 -0700

    Created donate feature, which accepts a text input from a user, writes the item and corresponding timestamp to the database, and then displays the fridge content page updated to include the user's added item.

--- scripts/contents.js
+++ scripts/contents.js
@@ -4,6 +4,20 @@ function toDateTime(secs) {
     return t;
 }
 
+function addContentToPage(contentItem) {
+    let item = contentItem.details;
+    let timestamp = contentItem.timestamp;
+
+    let date = toDateTime(timestamp.seconds);
+    let newcard = document.querySelector('.fridge-item-card').cloneNode(true);
+
+    newcard.classList.remove('d-none');
+    newcard.querySelector('.fridge-item-title').innerHTML = item;
+    newcard.querySelector('.timestamp').innerHTML = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+
+    document.querySelector('#fridge-content-container').appendChild(newcard);
+}
+
 function displayFridgeInfo() {
     let params = new URL(window.location.href); //get URL of search bar
     let ID = params.searchParams.get("docID"); //get value for key "id"
@@ -30,22 +44,40 @@ function displayFridgeInfo() {
         .then(docs => {
             docs.forEach(doc => {
                 let contentItem = doc.data();
-                let item = contentItem.details;
-                let timestamp = contentItem.timestamp;
-
-                let date = toDateTime(timestamp.seconds);
-                let newcard = document.querySelector('.fridge-item-card').cloneNode(true);
-
-                newcard.classList.remove('d-none');
-                newcard.querySelector('.fridge-item-title').innerHTML = item;
-                newcard.querySelector('.timestamp').innerHTML = date.toLocaleDateString() + " " + date.toLocaleTimeString();
-
-                document.querySelector('#fridge-content-container').appendChild(newcard);
-
-
+                
+                addContentToPage(contentItem);
             })
         })
 }
 displayFridgeInfo();
 
+function addContentToDatabase() {
+
+    let newItem = document.getElementById('donate-input').value;
+
+    let params = new URL(window.location.href);
+    let ID = params.searchParams.get("docID");
+
+    let date = new Date()
+
+    let contentItem = {
+        details: newItem,
+        timestamp: {
+            seconds: parseInt(date / 1000)
+        }
+    }
+
+    db.collection("fridges")
+        .doc(ID)
+        .collection("contents")
+        .doc()
+        .set(contentItem)
+        .then(() => {
+            addContentToPage(contentItem)
+        }).catch(_ => {
+            alert("An error has occurred");
+        })
+
+
+}
 
