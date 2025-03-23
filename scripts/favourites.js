7b745bf622f33eb3f5ecdd40bdbcd068e5d08bd7
Ana Silva <anajnsilva@gmail.com>
2025-03-23 14:55:04 -0700

    Minor fixes to favourites display page to include fridge address and link to back button

--- scripts/favourites.js
+++ scripts/favourites.js
@@ -37,7 +37,8 @@ function populateFavourites() {
 
                                         //update title and text and image
                                         newcard.querySelector('#fridge-title').innerHTML = fridgeData.name;
-                                        newcard.querySelector('#f-image').src = `./images/${fridgeData.code}.png`; //Example: NV01.png
+                                        newcard.querySelector('#f-image').src = `./images/${fridgeData.code}.png`; 
+                                        newcard.querySelector('#fridge-address').innerHTML = fridgeData.address;
                                         newcard.querySelector('a').href = "contents.html?docID=" + docID;
                                         document.getElementById("favourites-go-here").appendChild(newcard);
                                         
